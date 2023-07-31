918312f041c2a9cc37a24623cd3c8eec0f11f1dc
Moira Mcdermott <moimcder@publicisgroupe.net>
2023-07-31 17:08:30 -0400

    test

--- helpers/attraction.py
+++ helpers/attraction.py
@@ -2,73 +2,89 @@ from api_calls import getplaces_google_local_api
 from helpers.route_optimization import shortest_route
 import requests
 import json
+import pyshorteners
 
 api_url = "http://localhost:4000"
-api_request_headers  = {"Content-Type": "application/json"}
-# used to generate 5 places for tour
-# address = <number> <street>, <city>
+api_request_headers = {"Content-Type": "application/json"}
+
+
+# used to generate (at most) 5 places for tour
+# user_location = {'lat': <lat>, 'long': <long>, 'street_address': <address>}
 # location = city, state, country
-def generate_tour(location, user_location, user_phone_number):
-    prompt = "attractions within a 1 mile radius of " + user_location['street_address']
+def generate_tour(location, user_location, user_phone_number, preference):
+    if preference == "attractions":
+        prompt = (
+            "attractions within a 1 mile radius of " + user_location["street_address"]
+        )
+    else:
+        prompt = preference + " near " + user_location["street_address"]
     places, coords_dict = getplaces_google_local_api(prompt, location)
     num_attractions = min(5, len(coords_dict))
     coords_dict = coords_dict[:num_attractions]
     user_dict = {}
-    user_dict['title'] = "User location"
-    user_dict['coordinates'] = {'latitude': user_location['lat'], 'longitude': user_location['long']}
+    user_dict["title"] = "User location"
+    user_dict["coordinates"] = {
+        "latitude": user_location["lat"],
+        "longitude": user_location["long"],
+    }
     coords_dict.insert(0, user_dict)
+    print(coords_dict)
     attraction_route = shortest_route(coords_dict)
-    places_dict = {"places" : attraction_route}
+    places_dict = {"places": attraction_route}
     place_update_response = requests.patch(
         f"{api_url}/api/users/{user_phone_number}/places",
         headers=api_request_headers,
-        data=json.dumps(places_dict)
-        )
-    #print(place_update_response)
-    return [x['title'] for x in attraction_route], places_dict
+        data=json.dumps(places_dict),
+    )
+    print(place_update_response)
+    return [x["title"] for x in attraction_route], places_dict
 
-#retrieves 1 place from top of user's list
+
+# retrieves 1 place from top of user's list
 def view_place(user_phone_number):
     user_places_response = requests.get(
-        f"{api_url}/api/users/{user_phone_number}/places",
-        headers=api_request_headers
-        )
-    return (user_places_response.json()[0])
+        f"{api_url}/api/users/{user_phone_number}/places", headers=api_request_headers
+    )
+    return user_places_response.json()[0]
 
-def get_city(user_phone_number):
-    user_city_response = requests.get(f"{api_url}/api/users/{user_phone_number}/location", headers=api_request_headers)
-    user_loc = user_city_response.json()
-    return ((user_loc.get('street_address', 'Boston, MA')).split(",")[1]).strip()
-#checks whether user has more places to visit
+
 def tour_done(user_phone_number):
     user_places_response = requests.get(
-        f"{api_url}/api/users/{user_phone_number}/places",
-        headers=api_request_headers
-        )
+        f"{api_url}/api/users/{user_phone_number}/places", headers=api_request_headers
+    )
     return user_places_response.json() == []
 
-#removes first place from user's list
+
+def get_city(user_phone_number):
+    return "Boston"
+
+
 def remove_first(user_phone_number):
     get_place = requests.patch(
-        f"{api_url}/api/users/{user_phone_number}/places/remove", 
-        headers=api_request_headers
-        )
-    #print(get_place)
-    #print(type(get_place))
+        f"{api_url}/api/users/{user_phone_number}/places/remove",
+        headers=api_request_headers,
+    )
+    print(get_place)
+    print(type(get_place))
     if get_place.status_code == 200:
         place_response = get_place.json()
         area_name = place_response["place"]["title"]
-        return(area_name)
-    else: return("error")
+        return area_name
+    else:
+        return "error"
+
 
 def maps_link(attractions_dict, user_location):
-    google_url = 'https://www.google.com/maps/dir/'
+    google_url = "https://www.google.com/maps/dir/"
     user_coords = f"{user_location['lat']},{user_location['long']}"
-    places = attractions_dict['places']
-    attraction_plus_arr = [x['title'].replace(" ", "+") for x in places]
-    attractions_plus = ''
+    places = attractions_dict["places"]
+    attraction_plus_arr = [x["title"].replace(" ", "+") for x in places]
+    attractions_plus = ""
     for place in attraction_plus_arr:
-        attractions_plus += (place + '/')
+        attractions_plus += place + "/"
     end_coords = f"@{places[-1]['coordinates']['latitude']},{places[-1]['coordinates']['longitude']},15z"
-    link = f"{google_url}{user_coords}/{attractions_plus}{end_coords}"
-    return link
+    # long_url = f"{google_url}{user_coords}/{attractions_plus}{end_coords}"
+    long_url = f"{google_url}{user_coords}/{attractions_plus}"
+    type_tiny = pyshorteners.Shortener()
+    short_url = type_tiny.tinyurl.short(long_url)
+    return short_url
--- helpers/route_optimization.py
+++ helpers/route_optimization.py
@@ -1,21 +1,38 @@
 from haversine import haversine, Unit
-from api_calls import getplaces_google_local_api
+
 
 def shortest_route(locations):
     final_route = []
     final_route.append(locations[0])
-    i = 0
     while len(final_route) < 6:
-        minimum = 24000
+        minimum = 2500
         place = final_route[-1]
         locations.remove(place)
-        place_coords = (place['coordinates']['latitude'],
-                        place['coordinates']['longitude'])
+        place_coords = (
+            place["coordinates"]["latitude"],
+            place["coordinates"]["longitude"],
+        )
         for next_place in locations:
-            next_place_coords = (next_place['coordinates']['latitude'],
-                                 next_place['coordinates']['longitude'])
+            next_place_coords = (
+                next_place["coordinates"]["latitude"],
+                next_place["coordinates"]["longitude"],
+            )
             if haversine(place_coords, next_place_coords, unit=Unit.MILES) < minimum:
                 minimum = haversine(place_coords, next_place_coords, unit=Unit.MILES)
                 nearest = next_place
         final_route.append(nearest)
     return final_route[1:]
+
+
+# locations = [
+#     (42.3578, -71.0568), # current location
+#     (42.3521821, -71.0512911),  # Location 1 Boston Tea Party Ships and Museum
+#     (42.3587352,-71.05745399999999 ),  # Location 2 Old State House
+#     (42.3550897, -71.0657256),  # Location 3 Boston Common
+#     (42.363724999999995, -71.0536561),  # Location 4 Paul Revere House
+#     (42.3592478, -71.0491475),  # Location 5 New England Aquarium
+# ]
+
+
+# optimized_route = shortest_route(locations)
+# print(optimized_route)
